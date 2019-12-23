8880166ff2639625203f96c9cd5d2cf509982688
unknown <tereze.empele@gmail.com>
2019-12-23 09:47:53 +0200

    First commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Recruitment.API.ViewModels;
+using System.Threading.Tasks;
+
+namespace Recruitment.API.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SignInManager<IdentityUser> _signManager;
+        private UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager)
+        {
+            _userManager = userManager;
+            _signManager = signManager;
+        }
+
+        [HttpGet]
+        public ViewResult Register() {
+            return View();                
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Username };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication2.Models
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
